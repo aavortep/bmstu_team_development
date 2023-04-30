4a17ed189f97e52abd532a1d8e8372b7457d869c
Anna Petrova <nura.alexevna@yandex.ru>
2023-05-01 00:10:39 +0300

    [FR #22] Add links to header
    Add authorization links to the top of the start page.

--- artfolio-front/src/App.js
+++ artfolio-front/src/App.js
@@ -6,6 +6,16 @@ function App() {
     <div>
       <div className='Header-box'>
         Artfolio
+        <div id='links' style={{position: 'fixed', right: '15px', top: '5px'}}>
+          <a href='login'>
+            Зарегистрироваться
+          </a>
+        </div>
+        <div id='links' style={{position: 'fixed', right: '15px', top: '35px'}}>
+          <a href='signin'>
+            Войти
+          </a>
+        </div>
       </div>
     </div>
   );
