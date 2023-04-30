41f70ac4564d344b4b281f1b9ff1605a324140db
Anna Petrova <nura.alexevna@yandex.ru>
2023-05-01 02:19:26 +0300

    [FR #22] Make scroll bar
    Add scroll bar (albums feed) with temporary images without authors.

--- artfolio-front/src/App.js
+++ artfolio-front/src/App.js
@@ -17,6 +17,17 @@ function App() {
           </a>
         </div>
       </div>
+
+      <link href="dist/smooth-scrollbar.css" rel="stylesheet"/>
+      <script src="dist/smooth-scrollbar.js"></script>
+      <section scrollbar>
+        <div id = "feed_img" style={{float: 'left'}}><p>Альбом 1</p></div>
+        <div id = "feed_img" style={{marginLeft: '44px', float: 'left'}}><p>Альбом 2</p></div>
+        <div id = "feed_img" style={{marginLeft: '44px', float: 'left'}}><p>Альбом 3</p></div>
+        <div id = "feed_img" style={{marginTop: '44px', float: 'left'}}><p>Альбом 4</p></div>
+        <div id = "feed_img" style={{marginTop: '44px', marginLeft: '44px', float: 'left'}}><p>Альбом 5</p></div>
+        <div id = "feed_img" style={{marginTop: '44px', marginLeft: '44px', float: 'left'}}><p>Альбом 6</p></div>
+      </section>
     </div>
   );
 }
