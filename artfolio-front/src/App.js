e8b1bd5307e62f44b0ed176ee7c80da21feb5a67
Anna Petrova <nura.alexevna@yandex.ru>
2023-05-01 02:33:30 +0300

    [FR #22] Add authors
    Add temporary authors names to the albums.

--- artfolio-front/src/App.js
+++ artfolio-front/src/App.js
@@ -21,12 +21,30 @@ function App() {
       <link href="dist/smooth-scrollbar.css" rel="stylesheet"/>
       <script src="dist/smooth-scrollbar.js"></script>
       <section scrollbar>
-        <div id = "feed_img" style={{float: 'left'}}><p>Альбом 1</p></div>
-        <div id = "feed_img" style={{marginLeft: '44px', float: 'left'}}><p>Альбом 2</p></div>
-        <div id = "feed_img" style={{marginLeft: '44px', float: 'left'}}><p>Альбом 3</p></div>
-        <div id = "feed_img" style={{marginTop: '44px', float: 'left'}}><p>Альбом 4</p></div>
-        <div id = "feed_img" style={{marginTop: '44px', marginLeft: '44px', float: 'left'}}><p>Альбом 5</p></div>
-        <div id = "feed_img" style={{marginTop: '44px', marginLeft: '44px', float: 'left'}}><p>Альбом 6</p></div>
+        <div id = "feed_img" style={{float: 'left'}}>
+          <p style={{paddingTop: '190px', textAlign: 'left'}}>Иванов Иван</p>
+          <p>Альбом 1</p>
+        </div>
+        <div id = "feed_img" style={{marginLeft: '44px', float: 'left'}}>
+          <p style={{paddingTop: '190px', textAlign: 'left'}}>Иванов Иван</p>
+          <p>Альбом 2</p>
+        </div>
+        <div id = "feed_img" style={{marginLeft: '44px', float: 'left'}}>
+          <p style={{paddingTop: '190px', textAlign: 'left'}}>Иванов Иван</p>
+          <p>Альбом 3</p>
+        </div>
+        <div id = "feed_img" style={{marginTop: '64px', float: 'left'}}>
+          <p style={{paddingTop: '190px', textAlign: 'left'}}>Иванов Иван</p>
+          <p>Альбом 4</p>
+        </div>
+        <div id = "feed_img" style={{marginTop: '64px', marginLeft: '44px', float: 'left'}}>
+          <p style={{paddingTop: '190px', textAlign: 'left'}}>Иванов Иван</p>
+          <p>Альбом 5</p>
+        </div>
+        <div id = "feed_img" style={{marginTop: '64px', marginLeft: '44px', float: 'left'}}>
+          <p style={{paddingTop: '190px', textAlign: 'left'}}>Иванов Иван</p>
+          <p>Альбом 6</p>
+        </div>
       </section>
     </div>
   );
