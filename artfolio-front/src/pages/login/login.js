27427983c6b3c6a21f5e76de6ed115e6d3c53f71
Anna Petrova <nura.alexevna@yandex.ru>
2023-05-01 19:23:03 +0300

    [FR #25] Make registration page
    Make registartion page including:
    - header
    - registartion form (name, email, password, confirming password)
    - "register" button (redirect to feed page)
    - "to sign in" button
    - "home" ("feed") button (redirect to start page)
    - "search" button (redirect to search page)

--- artfolio-front/src/pages/login/login.js
+++ artfolio-front/src/pages/login/login.js
@@ -1,13 +1,66 @@
 import React from 'react';
 import '../../App.css';
+import { Link } from "react-router-dom";
 
 class LoginPage extends React.Component {
+    async handleLogInForm(event) {
+        //event.preventDefault()
+        //const data = new FormData(event.target)
+        //const response = await this.sendLogInData(data)
+        //if (response.status === 200) {
+        //    const body = await response.json()
+        //    alert("Успешная регистрация:\nТокен:" + body["token"])
+        //}
+        //else alert(response.status)
+    }
+
     render() {
         return (
             <div>
                 <div className='Header-box'>
                     Artfolio
                 </div>
+
+                <div>
+                    <a href='home'>
+                        <button type='button' className='Side-btn' style={{position: 'fixed', top: '97px', right: '0'}}>
+                            Лента
+                        </button>
+                    </a>
+                    <a href='search'>
+                        <button type='button' className='Side-btn' style={{position: 'fixed', top: '343px', right: '0'}}>
+                            Поиск
+                        </button>
+                    </a>
+                </div>
+
+                <form action="" id="register">
+                    <center>
+                        <input type="text" id="name" name="fio" placeholder="ФИО" className="Text-input"/>
+                    </center>
+                    <center>
+                        <input type="email" id="email" name="mail" placeholder="Email" className="Text-input"/>
+                    </center>
+                    <center>
+                        <input type="password" id="pass" name="password" placeholder="Пароль" className="Text-input"/>
+                    </center>
+                    <center>
+                        <input type="password" id="pass" name="password" placeholder="Подтвердите пароль" 
+                        className="Text-input"/>
+                    </center>
+                    <center>
+                        <Link to='/feed'>
+                            <button type="submit" className="Btn" onSubmit={this.handleLogInForm}>
+                                Зарегистрироваться
+                            </button>
+                        </Link>
+                    </center>
+                </form>
+                <center>
+                    <a href="/signin" className="Label-font">
+                        У меня уже есть аккаунт
+                    </a>
+                </center>
             </div>
         )
     }
