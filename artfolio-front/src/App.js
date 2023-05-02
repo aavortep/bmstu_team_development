376d0fa4565dacd2a65f27a37ea8ed2f93975a2d
Anna Petrova <nura.alexevna@yandex.ru>
2023-05-02 17:52:44 +0300

    [FR #25] Make registration page
    Make registartion page including:
    - header
    - registartion form (name, email, password, confirming password)
    - "register" button (redirect to feed page)
    - "to sign in" button
    - "home" ("feed") button (redirect to start page)
    - "search" button (redirect to search page)

--- artfolio-front/src/App.js
+++ artfolio-front/src/App.js
@@ -5,6 +5,7 @@ import HomePage from './pages/home/home.js';
 import LoginPage from './pages/login/login.js';
 import SigninPage from './pages/signin/signin.js';
 import SearchPage from './pages/search/search.js';
+import FeedPage from './pages/feed/feed.js';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
             <Route path='/login' element={<LoginPage/>} />
             <Route path='/signin' element={<SigninPage/>} />
             <Route path='/search' element={<SearchPage/>} />
+            <Route path='/feed' element={<FeedPage/>} />
             <Route exact path='/' element={<Navigate replace to='/home' />} />
           </Routes>
         </Router>
