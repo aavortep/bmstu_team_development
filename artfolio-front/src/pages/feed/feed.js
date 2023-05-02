1ad14007aacc7e3644b44c37b0f2ed558c496c95
Anna Petrova <nura.alexevna@yandex.ru>
2023-05-02 17:04:07 +0300

    [FR #22] Make feed page (authorized)
    Make feed page for authorized users including:
    - header
    - account in the corner (temporary name and photo)
    - albums feed with temporary images, names and authors
    - "search" button (redirect to search page)

--- artfolio-front/src/pages/feed/feed.js
+++ artfolio-front/src/pages/feed/feed.js
@@ -7,7 +7,53 @@ class FeedPage extends React.Component {
             <div>
               <div className='Header-box'>
                 Artfolio
+                <div id='links' style={{position: 'fixed', right: '55px', top: '20px'}}>
+                  <a href='portfolio'>
+                    Петр Петров
+                  </a>
+                </div>
+                <div id='round_img' style={{position: 'fixed', right: '15px', top: '20px'}}></div>
               </div>
+
+              <div>
+                <div id='pressed_btn' style={{position: 'fixed', top: '97px', right: '0'}}>
+                  <p>Лента</p>
+                </div>
+                <a href='search'>
+                  <button type='button' className='Side-btn' style={{position: 'fixed', top: '343px', right: '0'}}>
+                    Поиск
+                  </button>
+                </a>
+              </div>
+        
+              <link href="dist/smooth-scrollbar.css" rel="stylesheet"/>
+              <script src="dist/smooth-scrollbar.js"></script>
+              <section scrollbar>
+                <div id = "feed_img" style={{float: 'left'}}>
+                  <p style={{paddingTop: '190px', textAlign: 'left'}}>Иванов Иван</p>
+                  <p>Альбом 1</p>
+                </div>
+                <div id = "feed_img" style={{marginLeft: '44px', float: 'left'}}>
+                  <p style={{paddingTop: '190px', textAlign: 'left'}}>Иванов Иван</p>
+                  <p>Альбом 2</p>
+                </div>
+                <div id = "feed_img" style={{marginLeft: '44px', float: 'left'}}>
+                  <p style={{paddingTop: '190px', textAlign: 'left'}}>Иванов Иван</p>
+                  <p>Альбом 3</p>
+                </div>
+                <div id = "feed_img" style={{marginTop: '64px', float: 'left'}}>
+                  <p style={{paddingTop: '190px', textAlign: 'left'}}>Иванов Иван</p>
+                  <p>Альбом 4</p>
+                </div>
+                <div id = "feed_img" style={{marginTop: '64px', marginLeft: '44px', float: 'left'}}>
+                  <p style={{paddingTop: '190px', textAlign: 'left'}}>Иванов Иван</p>
+                  <p>Альбом 5</p>
+                </div>
+                <div id = "feed_img" style={{marginTop: '64px', marginLeft: '44px', float: 'left'}}>
+                  <p style={{paddingTop: '190px', textAlign: 'left'}}>Иванов Иван</p>
+                  <p>Альбом 6</p>
+                </div>
+              </section>
             </div>
         )
     }
